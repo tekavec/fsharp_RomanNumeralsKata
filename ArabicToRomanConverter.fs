6c88ef371fe53f8e86bb76c8097f324dff700484
Tomaž Tekavec <tomaz@codurance.com>
2015-10-23 14:43:55 +0100

    Added all test cases.

--- ArabicToRomanConverter.fs
+++ ArabicToRomanConverter.fs
@@ -1,6 +1,7 @@
 ﻿module ArabicToRomanConverter
 
-let ArabicAndRoman = [(10, "X"); (9, "IX"); (5, "V"); (4, "IV"); (1, "I")]
+let ArabicAndRoman = [(1000, "M"); (900, "CM"); (500, "D"); (400, "CD"); 
+    (100, "C"); (90, "XC"); (50, "L"); (40, "XL"); (10, "X"); (9, "IX"); (5, "V"); (4, "IV"); (1, "I")]
 
 let FindRoman arabic =
     ArabicAndRoman 
